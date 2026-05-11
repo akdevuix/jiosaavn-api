7ef0e530354a8d25e137cdb787e57642216c5f80
Ak.Dev <ag6443964@gmail.com>
2026-05-11 15:51:58 +0530

    boom for src folder

---
+++ modules/artists/models/index.ts
@@ -0,0 +1,4 @@
+export { ArtistModel, ArtistAPIResponseModel } from './artist.model'
+export { ArtistMapModel, ArtistMapAPIResponseModel } from './artist-map.model'
+export { ArtistSongModel, ArtistSongAPIResponseModel } from './artist-song.model'
+export { ArtistAlbumModel, ArtistAlbumAPIResponseModel } from './artist-album.model'
